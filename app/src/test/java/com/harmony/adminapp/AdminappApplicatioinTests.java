d8bd31133047604b7c6661a1caecf902ad6efbbe
Sylvia Lee <leesylviayy@gmail.com>
2024-01-22 22:33:47 -0700

    Initialized Springboot API framework

---
+++ app/src/test/java/com/harmony/adminapp/AdminappApplicatioinTests.java
@@ -0,0 +1,13 @@
+package com.harmony.adminapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
